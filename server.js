7a574fcf9f0ac1d39a54db4ba21f23b7695ecc57
pindardevelopment <pindardevelopment@gmail.com>
2025-03-13 00:40:11 +0700

    Update server.js add logging

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import "dotenv/config";
 import app from "./src/app.js";
 const PORT = process.env.PORT || 4000;
 
+console.log('[APP] Server running on http://localhost:4000');
+// Your logs will be tagged with [APP] for easy filtering
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
