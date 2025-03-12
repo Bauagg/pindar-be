32b67b6faa038eb971a395e38886122a18ab65aa
pindardevelopment <pindardevelopment@gmail.com>
2025-03-13 00:42:27 +0700

    Update server.js log again

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 import "dotenv/config";
 import app from "./src/app.js";
+
 const PORT = process.env.PORT || 4000;
 
-console.log('[APP] Server running on http://localhost:4000');
-// Your logs will be tagged with [APP] for easy filtering
-
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`[APP] Server running on http://localhost:${PORT}`);
+}).on('error', (err) => {
+  console.error(`[APP] Server error: ${err.message}`);
 });
+
+// You can also add more tagged logs throughout your application:
+// For errors:
+console.error('[APP] Error message');
+
+// For info:
+console.log('[APP] Info message');
+
+// For warnings:
+console.warn('[APP] Warning message');
+
+// For debug:
+console.debug('[APP] Debug message');
